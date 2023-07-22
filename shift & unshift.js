f76c7cbcadcfd6357c126b306648258fe274537d
DEEPAK KUMAR <112198021+29deepak@users.noreply.github.com>
2023-07-22 19:30:41 +0530

    Create shift & unshift.js

---
+++ shift & unshift.js
@@ -0,0 +1,12 @@
+const arr=[10,20,30,40,50]
+
+arr.shift() //--delete the first elementof the array
+
+console.log(arr) //[ 20, 30, 40, 50 ]
+
+
+arr.unshift(45)//-----add the values at the first element of the array
+console.log(arr) //--[ 45, 20, 30, 40, 50 ]
+
+arr.unshift(96)
+console.log(arr) //--[ 96, 45, 20, 30, 40, 50 ]
