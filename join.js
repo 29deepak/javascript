b369798933d7217c0abab43e374914fafdef3ae6
DEEPAK KUMAR <112198021+29deepak@users.noreply.github.com>
2023-07-22 19:39:01 +0530

    Create join.js

---
+++ join.js
@@ -0,0 +1,12 @@
+const arr=[10,20,30,40,50]
+let a =arr.join()
+console.log(a) //it return the elemenet ----10,20,30,40,50
+
+
+const b=arr.join("")
+console.log(b) //output--1020304050 --they are string
+
+
+
+const c= arr.join(" ")
+console.log(c) // ---10 20 30 40 50--they are string
