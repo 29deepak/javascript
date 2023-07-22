6c3a9769f37161d1ea087db466c3f7fca24625a3
DEEPAK KUMAR <112198021+29deepak@users.noreply.github.com>
2023-07-22 21:45:09 +0530

    Create fill and valueof.js

---
+++ fill and valueof.js
@@ -0,0 +1,13 @@
+const arr=[1,2,3,45,6,34]
+const b=arr.toString()
+console.log(b) //1,2,3,45,6,34
+
+
+const c =arr.valueOf()
+console.log(c) //output-[ 1, 2, 3, 45, 6, 34 ]
+
+//-------------------------fill--------------------------------
+
+var a=[1,2,3,5,6,7]
+a.fill(2)
+console.log(a) //output-[ 2, 2, 2, 2, 2, 2 ]
