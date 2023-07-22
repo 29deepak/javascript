c7f1f2254bfce3bc81731db03f7676458ff23298
DEEPAK KUMAR <112198021+29deepak@users.noreply.github.com>
2023-07-22 21:40:05 +0530

    Create includes.js

---
+++ includes.js
@@ -0,0 +1,11 @@
+const arr=[1,2,3,45,6,34]
+var b=!arr.includes(100)
+console.log(b) //output-true
+
+
+var c=arr.includes(34)
+console.log(c) //output-true
+
+
+var d=arr.includes(1000)
+console.log(d) //output--false
